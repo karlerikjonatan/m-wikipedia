72cd1017c3d3f19b8bc0bbc8eb892deb2c9f74b5
Jonatan Pettersson <pettersson.jonatan@gmail.com>
2019-07-07 15:51:43 +0200

    Updates

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const hash = window.location.hash;
+const pathname = window.location.pathname;
+const protocol = window.location.protocol;
+
+const lang = document.querySelector('html').lang;
+
+window.location.replace(`${protocol}//${lang}.m.wikipedia.org${pathname}${hash}`);
--- wiki-mobile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var protocol = window.location.protocol;
-var pathname = window.location.pathname;
-var hash = window.location.hash;
-
-var lang = document.querySelector('html').lang;
-
-window.location.replace(protocol + '//' + lang + '.m.wikipedia.org' + pathname + hash);
