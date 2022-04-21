9aab73f00242aab823688b3d6690d6391907b6f7
Jonatan Pettersson <pettersson.jonatan@gmail.com>
2022-04-21 09:38:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 const hash = window.location.hash;
 const pathname = window.location.pathname;
 const protocol = window.location.protocol;
 
-const lang = document.querySelector('html').lang;
+const lang = document.querySelector("html").lang;
 
 window.location.replace(`${protocol}//${lang}.m.wikipedia.org${pathname}${hash}`);
